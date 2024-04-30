dad160b2fdb6f49adebefa09af144d4b38b6e485
Uğur KÜSMEN <ugur_kusmen@hotmail.com>
2024-04-30 22:08:46 +0300

    deleting console.log on app.js

--- src/app.js
+++ src/app.js
@@ -13,13 +13,6 @@ import getVisibleExpenses from './selectors/expenses'
 const store = configureStore();
 
 
-
-
-
-
-
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
-console.log(visibleExpenses)
 const jsx = (
      <Provider store={store}>
         <AppRouter/>
